4462f87c0f1264bbb3fbdceabb9ae23605f763df
HoangCode <you@example.com>
2022-09-13 14:47:05 +0700

    first commit

---
+++ Home/header_sci_1_0_0/js/header_sci_1_0_0.js
@@ -0,0 +1,16 @@
+document.getElementById("headerMenuBtn").addEventListener("click", () => {
+  document.getElementById("headerSideBar").classList.add("show");
+  document.getElementById("headerBg").style.display = "block";
+});
+document.getElementById("headerBg").addEventListener("click", () => {
+  document.getElementById("headerSideBar").classList.remove("show");
+  document.getElementById("headerBg").style.display = "none";
+});
+
+const menuItem = document.querySelectorAll(".header_sci_1_0_0__item");
+// console.log(menuItem);
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("open");
+  });
+});
